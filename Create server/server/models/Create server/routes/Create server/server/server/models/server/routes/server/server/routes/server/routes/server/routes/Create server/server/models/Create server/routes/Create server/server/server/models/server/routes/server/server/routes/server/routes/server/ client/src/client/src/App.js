06be3cce8b1388b9015b9e11cf9426fe02d91e41
rocketshipexchange-creator <altcointradeprofitwithrocketsh@gmail.com>
2026-03-14 05:47:03 +0100

    Create App.js

---
+++ Create server/server/models/Create server/routes/Create server/server/server/models/server/routes/server/server/routes/server/routes/server/routes/Create server/server/models/Create server/routes/Create server/server/server/models/server/routes/server/server/routes/server/routes/server/ client/src/client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Trading from './pages/Trading';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/trading" element={<Trading />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
